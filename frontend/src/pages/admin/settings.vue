c9b97276a5a22c9a456c7ef1852255c9d11cbcc4
Joshua Schnabel (PC) <github@joshua-schnabel.de>
2020-05-04 20:44:16 +0200

    Add pages

---
+++ frontend/src/pages/admin/settings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="title">Settings</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'dashboard',
+  components: {
+  },
+  meta: {
+    page: 'admin.settings'
+  }
+})
+</script>
+
+<style></style>
